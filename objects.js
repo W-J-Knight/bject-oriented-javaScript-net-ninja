8788a611a1c61324ea7897335bc203943e8fce6c
William Knight <William.Jeffery.Knight@protonmail.com>
2024-04-04 19:04:01 -0400

    Video 5 create objects using class constructor

--- objects.js
+++ objects.js
@@ -1,3 +1,16 @@
 class User {
-    
+    constructor(email,name){
+        this.email = email;
+        this.name;
+    }
 }
+
+let userOne = new User('ryu@ninjas.com', 'Ryu');
+let userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+console.log(userOne)
+console.log(userTwo)
+
+// the 'new' keyword
+// - create a new empty object {}
+// - sets the value of 'this' to be the new empty object
+// - calls the constructor method
