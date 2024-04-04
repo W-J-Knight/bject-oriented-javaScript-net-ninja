587c807db57db57ea143ae47d25c127b8e54c2c5
William Knight <William.Jeffery.Knight@protonmail.com>
2024-04-04 18:31:16 -0400

    Video 3 updating properies and methods.

--- objects.js
+++ objects.js
@@ -12,4 +12,21 @@ var userOne = {
 userOne.login();
 userOne.logout();
 
+// console.log(userOne.name)
+
+userOne.name = "john"
 console.log(userOne.name)
+console.log(userOne)
+
+var prop = 'name'
+
+// userOne.email = 'ryu2k18@ninjas.com';
+// userOne['email'] = 'ryu2k18@ninjas.com';
+// userOne[prop] = 'Ryuke';
+
+// console.log(userOne);
+
+userOne.age = 25;
+// userOne.logInfo = function(){
+//     console.log(this.name, this.age, this.email);
+// };
