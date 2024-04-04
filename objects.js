61efba607fcc8be46ddbd3e3a7d482fe6c8f03ac
William Knight <William.Jeffery.Knight@protonmail.com>
2024-04-04 19:42:43 -0400

    Video 9 see under the hood how class work

--- objects.js
+++ objects.js
@@ -1,47 +1,14 @@
-class User {
-    constructor(email,name){
-        this.email = email;
-        this.name = name;
-        this.score = 0;
-    }
-    login(){
-        console.log(this.email, 'just logged in')
-        return this
-    }
-    logout(){
-        console.log(this.email, 'just logged out')
-        return this
-    }
-    upScore(){
-        this.score++;
-        console.log(this.email, 'score is now', this.score)
-        return this
-    }
+function User(email, name){
+    this.email = email;
+    this.name = name;
+    this.online = false;
+    this.login = function(){
+        console.log(this.email, 'has logged in');
+    };
 }
 
-class Admin extends User{
-    deleteUser(user){
-        users = users.filter(u => {
-            return u.email != user.email
-        })
-    }
-}
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
 
-let userOne = new User('ryu@ninjas.com', 'Ryu');
-let userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
-var admin = new Admin("admin@ninjas.com", 'shaun')
-console.log(userOne)
-console.log(userTwo)
-
-// the 'new' keyword
-// - create a new empty object {}
-// - sets the value of 'this' to be the new empty object
-// - calls the constructor method
-
-userOne.login()
-userTwo.logout()
-userOne.upScore().login()
-var users = [userOne, userTwo, admin]
-
-admin.deleteUser(userOne)
-console.log(users)
+console.log(userOne);
+userTwo.login();
