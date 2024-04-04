9a28641adffd48f5dc8d952d159cd6eda5f1d361
William Knight <William.Jeffery.Knight@protonmail.com>
2024-04-04 17:28:31 -0400

    Video userOne object.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var userOne = {
+    email: 'ryu@ninjas.com',
+    name: 'Ryu',
+    login(){
+        console.log(this.email, 'has logged in');
+    },
+    logout(){
+        console.log(this.email, 'has logged out');
+    }
+};
+
+userOne.login();
+userOne.logout();
+
+console.log(userOne.name)
